462d0d6a82f0c0645b310df3fddb9e41889c164b
Ivan Gusarov <xopuct@gmail.com>
2020-05-18 13:35:15 +0300

    Basic game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    public Vector2 Direction;
+    public float Speed;
+
+    private void Update()
+    {
+        transform.position += new Vector3(Direction.x, Direction.y) * Speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("CollisionEnter");
+        if (collision.contactCount < 0)
+            return;
+        Direction = Vector3.Reflect(Direction, collision.contacts[0].normal);
+    }
+}
